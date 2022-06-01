1a9d4d26b3259e0144f4b61d931bf620e98254f5
onehousesilver <onehousesilver@naver.com>
2022-06-01 23:17:53 +0900

    interface를 사용한 prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import styled from "styled-components";
+import Circle from "./Circle";
 
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
 function App() {
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
 }
 
